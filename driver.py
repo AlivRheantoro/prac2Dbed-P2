d9341a688fc161a3d703bf7953391e9870c29f75
Aliv Arafaputra Rheantoro <a1924315@adelaide.edu.au>
2025-09-02 07:52:28 +0000

    read db

--- driver.py
+++ driver.py
@@ -8,10 +8,10 @@ d1.setUp()
 print("Reading data")
 d1.readData('aust_trim_code.csv')
 print("Data read complete")
-for i in d1.select_by_pcode("5063"):
-    print(i)
-locstor = d1.entropyCalc()
-print('{:2.3f}'.format(locstor))
+#for i in d1.select_by_pcode("5063"):
+    #print(i)
+#locstor = d1.entropyCalc()
+#print('{:2.3f}'.format(locstor))
 print("Test show all:")
 for row in d1.show_all():
     print(row)
