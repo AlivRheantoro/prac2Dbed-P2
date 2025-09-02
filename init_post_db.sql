d2acd4d0044595d49d5190944de12f73157ed7c1
Aliv Arafaputra Rheantoro <a1924315@adelaide.edu.au>
2025-09-02 06:53:37 +0000

    post code db added

---
+++ init_post_db.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS postal;
+CREATE DATABASE postal;
+USE postal;
+CREATE TABLE pcode (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    postcode VARCHAR(4) NOT NULL,
+    locality VARCHAR(40) NOT NULL,
+    state VARCHAR(3) NOT NULL
+);
